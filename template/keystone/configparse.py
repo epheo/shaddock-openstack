d1c7eba2e3bd0eb69487b47b886d9e8ba41f2efb
Epheo <root@epheo.eu>
2015-03-06 02:00:36 +0000

    fic ConfigParser

--- template/keystone/configparse.py
+++ template/keystone/configparse.py
@@ -15,7 +15,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from configparser import ConfigParser
+from configparse import ConfigParser
 import os
 
 configfile = '/etc/keystone/keystone.conf'
