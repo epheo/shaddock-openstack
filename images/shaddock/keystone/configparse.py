99f2c9e8d01b35439d3a4403be586ff1b5c4d222
Thibaut Lapierre <root@epheo.eu>
2015-05-18 13:22:11 +0100

    Fix block storage and keystone from sources

--- images/shaddock/keystone/configparse.py
+++ images/shaddock/keystone/configparse.py
@@ -61,4 +61,4 @@ keystone_conf = {
     {'driver': 'keystone.contrib.revoke.backends.sql.Revoke'}
     }
 
-apply_config('/opt/keystone/etc/keystone.conf', keystone_conf)
+apply_config('/etc/keystone/keystone.conf', keystone_conf)
