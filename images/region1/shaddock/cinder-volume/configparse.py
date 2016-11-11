11356d28eae347192ba30dda7513a74ed9890008
epheo <thibaut@hpe.com>
2016-11-11 02:27:33 +0000

    Work on pyvenv, vars, configparse and all start scripts

--- images/region1/shaddock/cinder-volume/configparse.py
+++ images/region1/shaddock/cinder-volume/configparse.py
@@ -19,11 +19,11 @@ import ConfigParser
 import os
 
 mysql_host_ip = os.environ.get('MYSQL_HOST_IP')
-keystone_host_ip = os.environ.get('KEYSTONE_HOST_IP')
-glance_host_ip = os.environ.get('GLANCE_HOST_IP')
+keystone_host_ip = os.environ.get('KEYSTONE_API_IP')
+glance_host_ip = os.environ.get('GLANCE_API_IP')
 rabbit_host_ip = os.environ.get('RABBIT_HOST_IP')
 rabbit_pass = os.environ.get('RABBIT_PASS')
-cinder_host_ip = os.environ.get('CINDER_HOST_IP')
+cinder_host_ip = os.environ.get('CINDER_API_IP')
 cinder_pass = os.environ.get('CINDER_PASS')
 cinder_db_pass = os.environ.get('CINDER_DBPASS')
 
