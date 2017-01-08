235303038ed412b2614985f73420e436c1a7be08
weezhard <weezhard@outlook.com>
2017-01-08 19:03:43 +0100

    Fix cinder-volume, disable udev in lvm.conf

--- images/region1/shaddock/cinder-volume/run.sh
+++ images/region1/shaddock/cinder-volume/run.sh
@@ -4,6 +4,11 @@ CINDER_PATH=/opt/openstack/services/cinder/bin/
 
 ln -s /opt/openstack/services/cinder/etc/cinder /etc/cinder
 
+sed -i "s/udev_sync = 1/udev_sync = 0/g" \
+  /etc/lvm/lvm.conf
+sed -i "s/udev_rules = 1/udev_rules = 0/g" \
+  /etc/lvm/lvm.conf
+
 echo "Updating conf file..."
 $CINDER_PATH/python /opt/configparse.py
 
